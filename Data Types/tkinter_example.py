a863790310ed73f1ce1b0a7454a3ac998f3b4e23
Fogs Consultants <fogsconsultants@gmail.com>
2026-04-30 14:36:42 +0530

    save

---
+++ Data Types/tkinter_example.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+window=Tk()
+
+def Signup():
+    print(f"Hi,{f_name.get()} {l_name.get()}")
+Label(text="First Name").grid(row=0,column=0)
+f_name=Entry()
+f_name.grid(row=0,column=1)
+
+Label(text="Last Name").grid(row=1,column=0)
+l_name=Entry()
+l_name.grid(row=1,column=1)
+
+Button(text="Register", command=Signup).grid(row=2,column=0,columnspan=2)
+window.mainloop()
