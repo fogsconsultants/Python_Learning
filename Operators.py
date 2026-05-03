b693847aacdd773942be06b38ce128d8136bf86f
Fogs Consultants <fogsconsultants@gmail.com>
2026-05-03 23:20:15 +0530

    save

---
+++ Operators.py
@@ -0,0 +1,70 @@
+"""#Arithmetic Operators
+a = 10
+b = 5
+print("Addition:", a + b)          # Output: 15
+print("Subtraction:", a - b)       # Output: 5
+print("Multiplication:", a * b)    # Output: 50
+print("Division:", a / b)          # Output: 2.0
+print("Floor Division:", a // b)  # Output: 2
+print("Modulus:", a % b)          # Output: 0
+print("Exponentiation:", a ** b)  # Output: 100000
+#Assignment Operators
+x = 10
+x += 5  # Equivalent to x = x + 5
+print("x += 5:", x)  # Output: 15
+x -= 3  # Equivalent to x = x - 3
+print("x -= 3:", x)  # Output: 12
+x *= 2  # Equivalent to x = x * 2
+print("x *= 2:", x)  # Output: 24
+x /= 4  # Equivalent to x = x / 4
+print("x /= 4:", x)  # Output: 6.0
+x //= 2  # Equivalent to x = x // 2
+print("x //= 2:", x)  # Output: 3.0
+x %= 2  # Equivalent to x = x % 2
+print("x %= 2:", x)  # Output: 1.0
+x **= 3  # Equivalent to x = x ** 3
+print("x **= 3:", x)  # Output: 1.0
+
+#Comparison Operators
+a = 10
+b = 5
+print("a == b:", a == b)  # Output: False
+print("a != b:", a != b)  # Output: True
+print("a > b:", a > b)    # Output: True
+print("a < b:", a < b)    # Output: False
+print("a >= b:", a >= b)  # Output: True
+print("a <= b:", a <= b)  # Output: False
+
+#Logical Operators
+x = True
+y = False
+print("x and y:", x and y)  # Output: False
+print("x or y:", x or y)    # Output: True
+print("not x:", not x)      # Output: False
+print("not y:", not y)      # Output: True
+
+#Bitwise Operators
+a = 5  # In binary: 0101
+b = 3  # In binary: 0011
+print("a & b:", a & b)  # Output: 1 (In binary: 0001)
+print("a | b:", a | b)  # Output: 7 (In binary: 0111)
+print("a ^ b:", a ^ b)  # Output: 6 (In binary: 0110)
+print("~a:", ~a)        # Output: -6 (In binary: 1010)
+print("a << 1:", a << 1)  # Output: 10 (In binary: 1010)
+print("a >> 1:", a >> 1)  # Output: 2 (In binary: 0010)"""
+
+#Membership Operators
+mylist=[10,20,30,40,50]
+print(10 in mylist)
+print(60 in mylist)
+print(10 not in mylist)
+print(60 not in mylist)
+
+txt="Hello World"
+print("w" in txt)
+print("W" in txt)
+print("w" not in txt)
+print("W" not in txt)
+
+
+
